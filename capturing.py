05487f569c659a12c9fd12ec955c2da49174c01e
peppermint-ice <d.usenko@hotmail.com>
2022-12-28 13:54:48 +0200

    #2

---
+++ capturing.py
@@ -0,0 +1,22 @@
+import cv2
+
+
+def capturing_frame(port, datetime_str):
+    cap = cv2.VideoCapture(port)  # video capture source camera (Here webcam of laptop)
+    ret, frame = cap.read()   # return a single frame in variable `frame`
+    while True:
+        # cv2.imshow('img1', frame)  # display the captured image
+        # if cv2.waitKey(1) & 0xFF == ord('y'):  # save on pressing 'y'
+        #     cv2.imwrite(''.join(['images/', datetime_str, '_c', str(port) + '.png']), frame)
+        #     cv2.destroyAllWindows()
+        #     break
+        cv2.imwrite(''.join(['images/', datetime_str, '_c', str(port) + '.png']), frame)
+        cv2.destroyAllWindows()
+        break
+
+    cap.release()
+
+
+
+
+
