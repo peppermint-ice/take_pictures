c888435b575c235bcfb6261fb6bc82bcc5f4767e
peppermint-ice <d.usenko@hotmail.com>
2022-12-28 15:02:44 +0200

    #2

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cv2
 from datetime import datetime
-from check_for_camears import List_Ports
+from check_for_camears import list_ports
 from capturing import capturing_frame
 
 dt = datetime.now()
 current_time = dt.strftime("%Y_%m_%d_%H_%M_%S")
 print(current_time)
 
-working_ports = List_Ports()[1]
+working_ports = list_ports()[1]
 print(working_ports)
 for i in working_ports:
     capturing_frame(i, current_time)
