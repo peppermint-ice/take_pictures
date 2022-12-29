41eb852bf732c575ab3f99077f1283c3f9153a5e
peppermint-ice <d.usenko@hotmail.com>
2022-12-29 14:29:34 +0200

    #2

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from check_for_cameras import list_ports
 from capturing import capturing_frame
 
 
-dt = datetime.now()
-current_time = dt.strftime("%Y_%m_%d_%H_%M_%S")
-print(current_time)
+while True:
+    dt = datetime.now()
+    current_time = dt.strftime("%Y_%m_%d_%H_%M_%S")
+    print(current_time)
 
-working_ports = list_ports()[1]
-print(working_ports)
-for i in working_ports:
-    capturing_frame(i, current_time)
-
+    working_ports = list_ports()[1]
+    print(working_ports)
+    for i in working_ports:
+        capturing_frame(i, current_time)
+    input("Press Enter to continue...")
